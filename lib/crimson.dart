3e10adef68645e62a0be52fe07c54d835e003b0e
Daniel Gallegos Alia <dani@theyellowfactory.com>
2021-11-04 19:57:50 +0100

    Add datetime_validator to the library

--- lib/crimson.dart
+++ lib/crimson.dart
@@ -3,3 +3,4 @@ library crimson;
 export 'src/generated/l10n/crimson_localizations.dart';
 export 'src/string_validator.dart';
 export 'src/number_validator.dart';
+export 'src/datetime_validator.dart';
